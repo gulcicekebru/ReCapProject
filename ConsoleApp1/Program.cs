ec299f40d147d0f6bd3e426a36b75128c9f7c0ce
Ebru Gülçiçek <52013451+gulcicekebru@users.noreply.github.com>
2023-08-17 18:26:53 +0300

    updated

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Desciption);
+            }
+
+            Console.WriteLine("----------------------------------");
+
+            Car car1 = new Car() { Desciption = "Hyundai",Id = 10,BrandId=3,ColorId=2,DailyPrice=350,ModelYear=DateTime.Now};
+            Car car2 = new Car() { Desciption = "Hyundaiupdated", Id = 10, BrandId = 3, ColorId = 2, DailyPrice = 750, ModelYear = DateTime.Now };
+
+            carManager.Add(car1);
+
+            Console.WriteLine("-----------New Car Added----------");
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id + " " + car.Desciption);
+            }
+
+            Console.WriteLine("-----------id 3 deleted ----------");
+            carManager.Delete(3);
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id + " " + car.Desciption);
+            }
+
+            Console.WriteLine("-----------id 10 updated----------");
+            carManager.Update(car2);
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id + " " + car.Desciption);
+            }
+
+            Console.WriteLine("-----------id 10 listed ----------");
+            Console.WriteLine(carManager.GetById(10).Desciption);
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,DailyPrice=500,Desciption="Renault",ModelYear=DateTime.Now},
+                new Car{Id=2,BrandId=2,ColorId=2,DailyPrice=1000,Desciption="BMW",ModelYear=DateTime.Now},
+                new Car{Id=3,BrandId=1,ColorId=3,DailyPrice=400,Desciption="Renault 308",ModelYear=DateTime.Now},
+                new Car{Id=4,BrandId=2,ColorId=4,DailyPrice=2000,Desciption="BMW 5.0",ModelYear=DateTime.Now}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(int id)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.Id==id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            Car carToGet = _cars.SingleOrDefault(c => c.Id == id);
+            return carToGet;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Desciption = car.Desciption;
+
+        }
+    }
+}
